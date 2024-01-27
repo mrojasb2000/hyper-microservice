4209c20161c508ca4627d0525195747a32a5db3a
Mauricio Rojas <mrojasb2000@gmail.com>
2024-01-26 22:33:49 -0300

    refactor: change port number

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use hyper::service::service_fn_ok;
 use hyper::{Body, Response, Server};
 
 fn main() {
-    let addr = ([127, 0, 0, 1], 8080).into();
+    let addr = ([127, 0, 0, 1], 3000).into();
     let builder = Server::bind(&addr);
     let server =
         builder.serve(|| service_fn_ok(|_| Response::new(Body::from("Almost microservice..."))));

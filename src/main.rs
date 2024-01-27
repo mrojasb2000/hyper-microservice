02c79231c2658c013fe97ce895a9db4a9304aae2
Mauricio Rojas <mrojasb2000@gmail.com>
2024-01-26 22:25:17 -0300

    feat: adding hyper basic microservice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server};
+
 fn main() {
-    println!("Hello, world!");
+    let addr = ([127, 0, 0, 1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server =
+        builder.serve(|| service_fn_ok(|_| Response::new(Body::from("Almost microservice..."))));
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
 }
